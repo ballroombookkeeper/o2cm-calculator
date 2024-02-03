313ee4c647517341782a2bd2fbca992559ae506e
ericblam <marcusgladir@gmail.com>
2024-02-03 12:51:06 -0500

    Update main routing to include assets

--- index.php
+++ index.php
@@ -1,11 +1,24 @@
 <?php
 
-$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$uri_exploded = explode('/', $uri);
+function is_asset(string $uri): bool {
+    return preg_match('/\.(?:png|jpg|jpeg|gif|css)$/', $uri);
+}
 
-if ((isset($uri_exploded[1]) && $uri_exploded[1] == 'api')) {
-    echo "Welcome to the API";
+function main(): bool {
+    $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    $uri_split = explode('/', $uri);
+    $path_info = pathinfo($uri);
+
+    if ((isset($uri_split[1]) && $uri_split[1] == 'api')) {
+        echo "Welcome to the API";
+    }
+    else if (is_asset($uri)) {
+        return false;
+    }
+    else {
+        include "./pages/home.php";
+    }
+    return true;
 }
-else {
-    include "./pages/home.php";    
-}
+
+return main();
