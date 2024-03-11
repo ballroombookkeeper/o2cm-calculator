296aaf64eabf493e5c73d2d0037a14fc47ae381b
ericblam <marcusgladir@gmail.com>
2024-03-10 23:34:40 -0400

    Rename tests

--- client/src/ballroom.test.ts
+++ client/src/ballroom.test.ts
@@ -1,57 +1,57 @@
 import { standardizeDance, standardizeStyle, Dance, Style, standardizeSkill, Skill } from "./ballroom";
 
-test('vwaltz1', () => {
+test('standardizeDance_vwaltz1', () => {
     const result = standardizeDance("Viennese Waltz");
     expect(result).toEqual(Dance.VWaltz);
 });
 
-test('vwaltz2', () => {
+test('standardizeDance_vwaltz2', () => {
     const result = standardizeDance("v Waltz");
     expect(result).toEqual(Dance.VWaltz);
 });
 
-test('vwaltz3', () => {
+test('standardizeDance_vwaltz3', () => {
     const result = standardizeDance("v. Waltz");
     expect(result).toEqual(Dance.VWaltz);
 });
 
-test('waltz', () => {
+test('standardizeDance_waltz', () => {
     const result = standardizeDance("Waltz");
     expect(result).toEqual(Dance.Waltz);
 });
 
-test('quickstep', () => {
+test('standardizeDance_quickstep', () => {
     const result = standardizeDance("quicKstep");
     expect(result).toEqual(Dance.Quickstep);
 });
 
-test('no dance', () => {
+test('standardizeDance_noDance', () => {
     const result = standardizeDance("ain't no dance here");
     expect(result).toEqual(null);
 });
 
-test('standard_standard', () => {
+test('standardizeStyle_standard_standard', () => {
     const result = standardizeStyle("standard");
     expect(result).toEqual(Style.Standard);
 });
 
-test('intl_standard', () => {
+test('standardizeStyle_intl_standard', () => {
     const result = standardizeStyle("intl.", [Dance.VWaltz]);
     expect(result).toEqual(Style.Standard);
 });
 
-test('international_standard', () => {
+test('standardizeStyle_international_standard', () => {
     const result = standardizeStyle("international", [Dance.VWaltz]);
     expect(result).toEqual(Style.Standard);
 });
 
 // TODO: Eventually, this may be acceptable
-test('americanquickstep_null', () => {
+test('standardizeStyle_americanquickstep_null', () => {
     const result = standardizeStyle("american", [Dance.Quickstep]);
     expect(result).toEqual(null);
 });
 
-test('internationalNoDance_null', () => {
+test('standardizeStyle_international_null', () => {
     const result = standardizeStyle("international");
     expect(result).toEqual(null);
 });
